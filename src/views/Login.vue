c18562b1cb7f6aae31558d61c68471b24afde68b
LiShuxue <1149926505@qq.com>
2019-06-29 15:40:18 +0800

    添加登陆验证码和记住密码功能

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-page">
-    <el-form :model="loginForm" :rules="loginRules" ref="loginForm" label-width="50px" class="login-form">
+    <el-form :model="loginForm" :rules="loginRules" ref="loginForm" label-width="70px" class="login-form">
       <h3 class="title">系统登录</h3>
 
       <el-form-item label="账号" prop="username">
@@ -11,7 +11,18 @@
         <el-input type="password" v-model="loginForm.password" placeholder="请输入密码" show-password></el-input>
       </el-form-item>
 
-      <el-checkbox v-model="remeberPass" checked class="remember">记住密码</el-checkbox>
+      <el-form-item label="验证码" prop="captcha">
+        <el-row>
+          <el-col :span="14">
+            <el-input type="text" v-model="loginForm.captcha" placeholder="请输入验证码"></el-input>
+          </el-col>
+          <el-col :span="10">
+            <img :src="captchaPath" @click="getCaptcha()" class="captcha-img">
+          </el-col>
+        </el-row>
+      </el-form-item>
+
+      <el-checkbox v-model="remeberPass" class="remember">记住密码</el-checkbox>
 
       <el-button type="primary" @click="submitLogin" class="login-button">登陆</el-button>
     </el-form>
@@ -21,13 +32,16 @@
 <script>
 import API from '@/ajax/api.js'
 import SHA256 from 'crypto-js/sha256'
+import { getUUID } from '@/utils'
 
 export default {
   data() {
     return {
       loginForm: {
         username: '',
-        password: ''
+        password: '',
+        captcha: '',
+        uuid: ''
       },
       loginRules: {
         username: [
@@ -35,11 +49,23 @@ export default {
         ],
         password: [
           { required: true, message: '请输入密码', trigger: 'blur' }
+        ],
+        captcha: [
+          { required: true, message: '请输入验证码', trigger: 'blur' }
         ]
       },
-      remeberPass: true
+      remeberPass: false,
+      captchaPath: ''
     }
   },
+
+  created() {
+    this.getCaptcha()
+    // 恢复密码
+    this.loginForm.username = localStorage.getItem('username')
+    this.loginForm.password = localStorage.getItem('password')
+  },
+
   methods: {
     submitLogin() {
       this.$refs['loginForm'].validate((valid) => {
@@ -49,11 +75,22 @@ export default {
             url: API.notRequireAuth.login.url,
             data: {
               username: this.loginForm.username,
-              password: SHA256(this.loginForm.password).toString()
+              password: SHA256(this.loginForm.password).toString(),
+              captcha: this.loginForm.captcha,
+              uuid: this.loginForm.uuid
             }
           }).then(response => {
             this.$message.success(response.data.msg)
             this.$store.dispatch('login', response.data)
+
+            // 保存密码
+            if (this.remeberPass) {
+              localStorage.setItem('username', this.loginForm.username)
+              localStorage.setItem('password', this.loginForm.password)
+            } else {
+              localStorage.removeItem('username')
+              localStorage.removeItem('password')
+            }
 
             // 根据传过来的参数，跳到不同的页面
             let redirect = decodeURIComponent(this.$route.query.redirect)
@@ -63,10 +100,16 @@ export default {
               this.$router.push('/home')
             }
           }).catch(err => {
+            this.getCaptcha()
             err && this.$message.error(err.data.msg)
           })
         }
       })
+    },
+
+    getCaptcha () {
+      this.loginForm.uuid = getUUID()
+      this.captchaPath = process.env.VUE_APP_API_URL + `/captcha.jpg?uuid=${this.loginForm.uuid}`
     }
   }
 }
@@ -90,6 +133,13 @@ export default {
 
     .remember{
       margin-left: 10px;
+    }
+
+    .captcha-img{
+      display: block;
+      width: 100%;
+      height: 40px;
+      padding-left: 5px;
     }
 
     .login-button{
