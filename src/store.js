ae0fa48c29c82f1abd90f15078dc5bc8d3957ee0
Li Shuxue <Shuxue.Li@sc.com>
2019-07-06 23:11:15 +0800

    根据用户角色role list动态添加路由

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { resetRoute } from '@/router'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     token: '',
-    username: ''
+    username: '',
+    userRoles: []
   },
 
   getters: { },
@@ -19,6 +21,11 @@ export default new Vuex.Store({
     saveUsername (state, username) {
       state.username = username
       sessionStorage.setItem('username', username)
+    },
+    saveUserRoles (state, roles) {
+      state.userRoles = roles
+      // session storage 不能存数组，只能存字符串。所以用+拼接数组的内容存起来，解析的时候再把它转换成数组
+      sessionStorage.setItem('userRoles', roles.join('+'))
     }
   },
 
@@ -26,10 +33,13 @@ export default new Vuex.Store({
     login ({ commit }, payload) {
       commit('saveToken', payload.token)
       commit('saveUsername', payload.username)
+      commit('saveUserRoles', payload.roles)
     },
     logout ({ commit }) {
       commit('saveToken', '')
       commit('saveUsername', '')
+      commit('saveUserRoles', [])
+      resetRoute()
     }
   }
 })
