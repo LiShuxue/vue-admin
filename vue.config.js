bdc73b9b677851eb679754e557d15be197c4914f
Li Shuxue <Shuxue.Li@sc.com>
2019-07-08 22:44:37 +0800

    用publicPath代替baseUrl。控制error code, fix some issue

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   /**
-   * You will need to set baseUrl if you plan to deploy your site under a sub path,
+   * You will need to set publicPath if you plan to deploy your site under a sub path,
    * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then baseUrl should be set to "/bar/".
+   * then publicPath should be set to "/bar/".
    * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/zh/config/#baseurl
    */
-  baseUrl: process.env.VUE_APP_BASEURL,
+  publicPath: process.env.VUE_APP_PUBLICPATH,
 
   chainWebpack(config) {}
 }
